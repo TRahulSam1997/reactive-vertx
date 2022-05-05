c406fff4994d52a6099aec1eb943df86b7df30cd
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-05-05 22:33:09 +1000

    initial loggin added

---
+++ src/main/java/com/reactive_vertx/reactive_vertx/Main.java
@@ -0,0 +1,18 @@
+package com.reactive_vertx.reactive_vertx;
+
+import io.vertx.core.Vertx;
+//import io.vertx.core.impl.logging.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.deployVerticle(new SensorVerticle());
+
+  }
+
+}
--- src/main/java/com/reactive_vertx/reactive_vertx/SensorVerticle.java
+++ src/main/java/com/reactive_vertx/reactive_vertx/SensorVerticle.java
@@ -1,11 +1,55 @@
 package com.reactive_vertx.reactive_vertx;
 
 import io.vertx.core.AbstractVerticle;
-import io.vertx.core.impl.logging.LoggerFactory;
+import io.vertx.core.Promise;
+//import io.vertx.core.impl.logging.LoggerFactory;
 
-import java.util.logging.Logger;
+import java.util.Random;
+import java.util.UUID;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class SensorVerticle extends AbstractVerticle {
 
-  private static final Logger logger = (Logger) LoggerFactory.getLogger(SensorVerticle.class);
+  private static final Logger logger = LoggerFactory.getLogger(SensorVerticle.class);
+  private static final int httpPort = Integer.parseInt(System.getenv().getOrDefault(
+    "HTTP_PORT", "8080"
+  ));
+
+  private final String uuid = UUID.randomUUID().toString();
+  private double temperature = 21.0;
+  private final Random random = new Random();
+
+  @Override
+  public void start(Promise<Void> startPromise) {
+    vertx.setPeriodic(2000, this::updateTemperature);
+
+    startPromise.complete();
+  }
+
+  private void updateTemperature(Long id) {
+    temperature = temperature + (random.nextGaussian() / 2.0d);
+//    logger.info("Temperature updated: {} ", temperature);
+
+    System.out.println("Temperature updated: {} " + temperature);
+  }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
