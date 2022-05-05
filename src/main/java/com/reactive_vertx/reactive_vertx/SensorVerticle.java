041fb13629df3c5ef871547cdd20b4da2ea2996e
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-05-05 16:01:00 +1000

    vertx init

---
+++ src/main/java/com/reactive_vertx/reactive_vertx/SensorVerticle.java
@@ -0,0 +1,11 @@
+package com.reactive_vertx.reactive_vertx;
+
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.impl.logging.LoggerFactory;
+
+import java.util.logging.Logger;
+
+public class SensorVerticle extends AbstractVerticle {
+
+  private static final Logger logger = (Logger) LoggerFactory.getLogger(SensorVerticle.class);
+}
