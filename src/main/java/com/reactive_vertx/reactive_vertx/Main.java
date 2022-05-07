86e9dd65f4bc89c4e1f9470794d279a29808d0b8
TRahulSam1997 <rahulsamaranayake@yahoo.com>
2022-05-07 23:08:33 +1000

    hazelcast clustering management added

--- src/main/java/com/reactive_vertx/reactive_vertx/Main.java
+++ src/main/java/com/reactive_vertx/reactive_vertx/Main.java
@@ -2,7 +2,10 @@ package com.reactive_vertx.reactive_vertx;
 
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
+import io.vertx.core.VertxOptions;
 import io.vertx.core.json.JsonObject;
+import io.vertx.core.spi.cluster.ClusterManager;
+import io.vertx.spi.cluster.hazelcast.HazelcastClusterManager;
 //import io.vertx.core.impl.logging.LoggerFactory;
 
 //import org.slf4j.Logger;
@@ -14,13 +17,24 @@ public class Main {
   public static void main(String[] args) {
     System.out.println("Starting app... ");
 
-    Vertx vertx = Vertx.vertx();
-    vertx.deployVerticle(new SensorVerticle(), new DeploymentOptions().setInstances(1));
+//    Vertx vertx = Vertx.vertx();
+//    vertx.deployVerticle(new SensorVerticle(), new DeploymentOptions().setInstances(1));
 //    vertx.deployVerticle("SensorVerticle", new DeploymentOptions().setInstances(1));
 
-    vertx.eventBus()
-      .<JsonObject>consumer("temperature.updates", jsonObjectMessage -> {
-        System.out.println(">>> {}" + jsonObjectMessage.body().encodePrettily());
+//    vertx.eventBus()
+//      .<JsonObject>consumer("temperature.updates", jsonObjectMessage -> {
+//        System.out.println(">>> {}" + jsonObjectMessage.body().encodePrettily());
+//      });
+    // Configuration code (omitted)
+    ClusterManager mgr = new HazelcastClusterManager();
+    VertxOptions options = new VertxOptions().setClusterManager(mgr);
+
+    Vertx.clusteredVertx(options)
+      .onSuccess(vertx -> {
+        vertx.deployVerticle(new SensorVerticle());
+      })
+      .onFailure(failure -> {
+        System.out.println("Woops " + failure);
       });
   }
 
